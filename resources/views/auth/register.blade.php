1b85b9574b31dcd5c1991a0e7c7053b442195c46
galleria <tra202505.isogai@gmail.com>
2025-10-06 07:21:30 +0900

    ユーザー登録画面作成終了

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,9 +8,8 @@
 <div class="main-content">
 {{-- ここの中にコードを書く --}}
 
-
-
-
+{{-- ============================================================================ --}}
+{{-- ============================================================================ --}}
 
 <div class="background">
   <div class="allfonts">
@@ -23,62 +22,70 @@
       <p>アカウントを作成して、<br class="br-sp">猫との素敵な出会いを始めましょう</p>
     </div>
 
-    <form action="{{ route('register') }}" method="POST">
+    <div class="form-container">
+      <form action="{{ route('register') }}" method="POST">
       @csrf
+
+    <div class="form-group">
       <div class="labelfonts">
-        <label for="name">ユーザー名</label>
+        <label for="name"><div class="text-left">ユーザー名</div></label>
       </div>
         <input type="text" class="textbox" id="name" name="name" placeholder="氏名を入力してください" value="{{ old('name') }}" />
+    </div>
 
+    <div class="form-group">
       <div class="labelfonts">
-        <label for="email">メールアドレス</label>
+        <label for="email"><div class="text-left">メールアドレス</div></label>
       </div>
         <input type="email" class="textbox" id="email" name="email" placeholder="メールアドレスを入力してください" value="{{ old('email') }}" />
+      </div>
 
+    <div class="form-group">
       <div class="labelfonts">
-        <label for="password">パスワード</label>
+        <label for="password"><div class="text-left">パスワード</div></label>
       </div>
         <input type="password" class="textbox" id="password" name="password" placeholder="8文字以上のパスワードを入力してください">
+    </div>
 
+    <div class="form-group">
       <div class="labelfonts">
-        <label for="password-confirm">パスワード（確認）</label>
-        </div>
-        <input type="password" class="textbox" id="password-confirm" name="password_confirmation" placeholder="パスワードを再入力してください">
-
-      {{-- ======================================== --}}
-      {{-- バリデーションメッセージを表示するためのもの --}}
-      @if($errors->any())
-        <div class="alert-danger">
-          @foreach($errors->all() as $message)
-            <p>{{ $message }}</p>
-          @endforeach
-        </div>
-          @endif
-      {{-- ======================================== --}}
-
-      <div class="labelfonts">
-
-      <div class="terms-text">
-        <p><a href=""><span>利用規約</span></a>
-        <br class="br-sp">と<br class="br-sp">
-        <a href=""><span>プライバシーポリシー</span></a>
-        <br class="br-sp">に同意します</p>
+        <label for="password-confirm"><div class="text-left">パスワード（確認）</div></label>
       </div>
+        <input type="password" class="textbox" id="password-confirm" name="password_confirmation" placeholder="パスワードを再入力してください">
+    </div>
 
-        <button type="submit" class="botten">アカウント作成</button>
+{{-- ======================================== --}}
+{{-- バリデーションメッセージを表示するためのもの --}}
+@if($errors->any())
+  <div class="alert-danger">
+@foreach($errors->all() as $message)
+  <p>{{ $message }}</p>
+@endforeach
+  </div>
+@endif
+{{-- ======================================== --}}
 
-    </form>
+    <div class="terms-text">
+      <p><a href=""><span>利用規約</span></a>
+      <br class="br-sp">と<br class="br-sp">
+      <a href=""><span>プライバシーポリシー</span></a>
+      <br class="br-sp">に同意します</p>
+    </div>
+
+    <button type="submit" class="botten">アカウント作成</button>
+
+      </form>
+    </div>
 
   <div class="login-prompt">
-    <p>すでにアカウントを<br class="br-sp">お持ちの方は<br class="br-sp"><a href=""><span>ログイン</span></a></p>
+    <p>すでにアカウントを<br class="br-sp">お持ちの方は<br class="br-sp"><a href="{{ route('login') }}"><span>ログイン</span></a></p>
   </div>
 
   </div>
 </div>
 
-
-
-
+{{-- ============================================================================ --}}
+{{-- ============================================================================ --}}
 
 </div>
 @endsection
