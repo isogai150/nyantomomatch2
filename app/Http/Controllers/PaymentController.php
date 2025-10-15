5228188760b920a388328194b2b5ee6c9889a44f
TomiokaKoudai <tra202505.tomioka@gmail.com>
2025-10-15 12:14:28 +0900

    カート情報表示機能作成完了（ボタンのaction属性は「#」なので決済入力ページのルーティングの作成完了後「#」からルーティングに書き換える）

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PaymentController extends Controller
 {
-    public function cart
+    // カート情報表示機能
+    public function showcart($postId)
+    {
+        $post = Post::findOrFail($postId);
+
+        return view('payment.cart', compact('post'));
+    }
 }
---
+++ resources/views/payment/cart.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/payment/cart.css') }}">
+@endsection
+
+@section('content')
+<div class="main-content">
+  <div class="main-top">
+    <p>決済システム</p>
+    <a href="{{ route('posts.index') }}">topページ</a>
+  </div>
+  <div class="main-center">
+    <div class="center-top">
+      <h2>安全で簡単な決済システム</h2>
+      <p>stripeを使用した信頼性の高い決済処理</p>
+    </div>
+    <div class="center-items">
+      <div class="item-left">
+        <h3>支払い内容</h3>
+        @if($post)
+        <div class="cart-inf">
+          <p>譲渡費用<span>{{ $post->cost_class }}</span></p>
+          {{-- action属性の中身はリーティングで購入画面ページのルーティングを作成次第追加する --}}
+          <form action="#" method="POST">
+            @csrf
+            <input type="hidden" name="post" value="{{ $post->id }}">
+            <button type="submit" class="payment-btn">購入画面へ</button>
+          </form>
+        </div>
+        @else
+        <p>購入情報がありません</p>
+        @endif
+      </div>
+      <div class="item-right">
+        <div class="security-top">
+          <h3>セキュリティ</h3>
+          <p>安全な決済処理を保証</p>
+        </div>
+        <div class="security-items">
+          <ul>
+            <li>SSL暗号化通信</li>
+            <li>PCI DSS準拠</li>
+            <li>3Dセキュア対応</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
