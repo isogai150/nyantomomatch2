00d83eac473b4f580389a7fd4d7bc43ba876106a
TomiokaKoudai <tra202505.tomioka@gmail.com>
2025-10-30 15:37:09 +0900

    管理者機能の整理

--- config/adminlte.php
+++ config/adminlte.php
@@ -329,12 +329,9 @@ return [
         ['header' => '管理メニュー'],
 
         [
-            'text' => '投稿関連',
-            'icon' => 'fas fa-cat',
-            'submenu' => [
-                ['text' => '投稿一覧', 'url' => 'admin/posts', 'icon' => 'fas fa-list'],
-                ['text' => '投稿通報一覧', 'url' => 'admin/post-reports', 'icon' => 'fas fa-flag'],
-            ],
+            'text' => '投稿通報一覧',
+            'url' => 'admin/post-reports',
+            'icon' => 'fas fa-flag',
         ],
         [
             'text' => 'DM関連',
@@ -361,11 +358,6 @@ return [
         ],
 
         ['header' => 'システム設定'],
-        [
-            'text' => 'マイページ',
-            'url'  => 'admin/profile',
-            'icon' => 'fas fa-user',
-        ],
 [
     'text' => 'ログアウト',
     'icon' => 'fas fa-sign-out-alt',
